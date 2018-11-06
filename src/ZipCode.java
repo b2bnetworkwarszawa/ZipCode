ca07d7b6dffdb0a7e1aea233f62ab23b7c1e93ad
heso6 <romaniuk99@gmail.com>
2018-11-06 13:27:41 +0100

    Initial commit

---
+++ src/ZipCode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ZipCode {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Boolean adress = sc.hasNext("\\d{2}-\\d{3}");
+
+    }
+}
+
+
+
